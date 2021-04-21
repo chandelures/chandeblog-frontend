2f2ad06737d0ae8206618b5384759c0fbabe1b8a
chandelures <chandelurewang@gmail.com>
2021-04-21 17:43:03 +0800

    fix: author name display is incorrect

--- components/ArticleDetail.vue
+++ components/ArticleDetail.vue
@@ -5,7 +5,7 @@
       <v-avatar class="mr-3" size="35">
         <img alt="" :src="avatar" />
       </v-avatar>
-      <span class="green--text text--darken-3 mr-3">{{ author }}</span>
+      <span class="green--text text--darken-3 mr-3">{{ author_name }}</span>
       <span class="grey--text text--darken-2">{{ created | formatDate }}</span>
     </div>
     <!-- eslint-disable vue/no-v-html -->
@@ -30,7 +30,7 @@ export default {
       type: String,
       default: '',
     },
-    author: {
+    author_name: {
       type: String,
       default: '',
     },
