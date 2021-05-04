fceff4b657776791071675e6ac460872770db9a0
chandelures <chandelurewang@gmail.com>
2021-05-04 17:53:44 +0800

    perf: update app bar

--- components/AppBar.vue
+++ components/AppBar.vue
@@ -1,28 +1,49 @@
 <template>
-  <v-app-bar color="green darken-3" dark min-height="65">
-    <v-container class="fill-height" fluid>
-      <v-row>
-        <v-col md="8" cols="12" class="d-flex mx-auto">
-          <nuxt-link class="my-auto mx-auto mr-lg-4 mr-md-2" to="/">
-            <v-img alt="CBLOG" src="../img/logo.png" width="100" contain>
-            </v-img>
-          </nuxt-link>
-          <v-btn
-            v-for="(link, index) in links"
+  <div>
+    <v-app-bar color="green darken-3" dark hide-on-scroll>
+      <v-container fluid>
+        <v-row>
+          <v-col md="8" cols="12" class="d-flex mx-auto px-0">
+            <v-app-bar-nav-icon
+              class="d-md-none my-auto"
+              @click.stop="drawer = !drawer"
+            ></v-app-bar-nav-icon>
+            <nuxt-link class="my-auto mx-auto mr-lg-4 mr-md-2" to="/">
+              <v-img alt="CBLOG" src="../img/logo.png" width="100" contain>
+              </v-img>
+            </nuxt-link>
+            <v-btn
+              v-for="(link, index) in links"
+              :key="index"
+              :to="link.to"
+              :ripple="false"
+              class="d-none d-md-flex my-auto"
+              text
+              plain
+            >
+              {{ link.text }}
+            </v-btn>
+            <v-spacer class="d-none d-md-flex"> </v-spacer>
+            <v-btn class="d-md-none" fab color="transparent" disabled plain>
+            </v-btn>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-app-bar>
+    <v-navigation-drawer v-model="drawer" absolute color="green darken-3" dark>
+      <v-list nav>
+        <v-list-item-group v-model="group">
+          <v-list-item
+            v-for="(item, index) in links"
             :key="index"
-            :to="link.to"
-            :ripple="false"
-            class="d-none d-md-flex"
-            text
-            plain
+            :to="item.to"
           >
-            {{ link.name }}
-          </v-btn>
-          <v-spacer class="d-none d-md-flex"> </v-spacer>
-        </v-col>
-      </v-row>
-    </v-container>
-  </v-app-bar>
+            <v-list-item-title v-text="item.text"></v-list-item-title>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
+  </div>
 </template>
 
 <script>
@@ -30,17 +51,18 @@ export default {
   name: 'AppBar',
   data() {
     return {
+      drawer: false,
+      group: null,
       links: [
-        { name: 'Home', to: '/' },
-        { name: 'About', to: '/about' },
+        { text: 'Home', to: '/' },
+        { text: 'About', to: '/about' },
       ],
     }
   },
+  watch: {
+    group() {
+      this.drawer = false
+    },
+  },
 }
 </script>
-
-<style scoped>
-.v-toolbar {
-  flex: 0 1 auto;
-}
-</style>
