87ac5591c61fceee79dd749c172995ea1d441651
chandelures <chandelurewang@gmail.com>
2021-05-11 19:23:17 +0800

    fix: update sign up from validate

--- pages/signup.vue
+++ pages/signup.vue
@@ -1,10 +1,11 @@
 <template>
   <v-container class="text-center my-12">
     <div class="text-h5 my-12">注册 Clog</div>
-    <form class="mx-auto" lazy-validation @submit.prevent="signUp">
+    <v-form ref="form" class="mx-auto" lazy-validation @submit.prevent="signUp">
       <v-text-field
         v-model="signup.username"
         type="text"
+        :rules="usernameRules"
         label="用户名"
         color="black"
         required
@@ -22,6 +23,7 @@
       <v-text-field
         v-model="signup.password"
         type="password"
+        :rules="passwordRules"
         label="密码"
         color="black"
         required
@@ -37,7 +39,7 @@
       >
       </v-text-field>
       <v-btn type="submit" class="green darken-3 my-2" dark>注册 </v-btn>
-    </form>
+    </v-form>
   </v-container>
 </template>
 
@@ -53,8 +55,18 @@ export default {
         email: '',
         password: '',
       },
-      emailRules: [(value) => /.+@.+\..+/.test(value) || 'E-mail格式不正确'],
-      passwordRules: [(value) => value.length >= 8 || '至少需要8个字符'],
+      usernameRules: [
+        (value) => !!value || '用户名不能为空',
+        (value) => value.length < 10 || '用户名不超过10个字符',
+      ],
+      emailRules: [
+        (value) => !!value || '邮箱不能为空',
+        (value) => /.+@.+\..+/.test(value) || 'E-mail格式不正确',
+      ],
+      passwordRules: [
+        (value) => !!value || '密码不能为空',
+        (value) => value.length >= 8 || '至少需要8个字符',
+      ],
       passwordCheckRules: [
         (value) => value === this.signup.password || '两次输入密码不一致',
       ],
@@ -63,19 +75,21 @@ export default {
   },
   methods: {
     async signUp() {
-      try {
-        await this.$axios.$post('auth/register', this.signup)
-        const login = {
-          username: this.signup.username,
-          password: this.signup.password,
+      if (this.$refs.form.validate()) {
+        try {
+          await this.$axios.$post('auth/register', this.signup)
+          const login = {
+            username: this.signup.username,
+            password: this.signup.password,
+          }
+          await this.$auth.loginWith('local', { data: login })
+          if (this.$auth.loggedIn) {
+            this.$router.go(0)
+          }
+          this.$toast.success('注册成功')
+        } catch (err) {
+          this.$toast.error('注册失败')
         }
-        await this.$auth.loginWith('local', { data: login })
-        if (this.$auth.loggedIn) {
-          this.$router.go(0)
-        }
-        this.$toast.success('注册成功')
-      } catch (err) {
-        this.$toast.error('注册失败')
       }
     },
   },
